71b55ad00c490342d825f9fa8b5eff660c05299d
Nestor <nbolivar@unal.edu.co>
2025-08-28 15:11:13 -0500

    first commit

---
+++ test.py
@@ -0,0 +1,19 @@
+#Autor 
+#Primera version
+
+#1. Estructuras de control
+#if, for, while, switch, dowhile
+
+#Prueba 
+# estructura un programa que mencione si su calificaccion es meritoria
+#4.5   5 meritorio
+
+Nota = float(input("Escriba nota en forma numerica entre 0 5"))
+
+if(Nota >= 4.5 ):
+    print("Esta nota es meritoria")
+
+
+
+
+
